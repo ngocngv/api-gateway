5e9fdca2b2c5e8f042dadd449bc2630be5df715f
System <Mr.NN91@gmail.com>
2016-12-21 09:42:52 +0700

    Create development-environment.py

---
+++ docs/development-environment.py
@@ -0,0 +1,16 @@
+
+
+# http://wickedhaufeio.readthedocs.io/en/latest/development-environment/#debugging-messages
+
+
+# Debugging messages
+
+# The following debugging scopes (DEBUG=) can be useful to know:
+
+portal:*: For all portal related debugging messages
+portal:auth:*: For all authentication related messages
+portal-api:*: Portal API debugging
+kong-adapter:*: Kong Adapter debugging
+portal-mailer:*: Mailer debugging
+portal-chatbot:*: Chatbot debugging
+wicked-sdk: Debugging messages from the wicked SDK, which is the base for kong adapter, mailer and chatbot (and partly the portal)
